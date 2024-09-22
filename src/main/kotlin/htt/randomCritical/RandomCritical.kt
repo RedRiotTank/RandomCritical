5c797198f73eb9eade3e02c05ea407a8e3d9351d
AlbertoPlaza <albertoplazamontesdm@gmail.com>
2024-09-22 23:08:13 +0200

    FIRST COMMIT

---
+++ src/main/kotlin/htt/randomCritical/RandomCritical.kt
@@ -0,0 +1,14 @@
+package htt.randomCritical
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RandomCritical : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
